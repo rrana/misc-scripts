481517185ef75e99eb6d476434055f58219dacc9
rrana <rana@newscred.com>
2016-11-13 17:18:49 +0600

    Again, remove hardcoded date

--- python/days-until-payday.py
+++ python/days-until-payday.py
@@ -4,7 +4,7 @@
 import datetime
 from calendar import monthrange
 
-payday_is = 25
+payday = 25 #th of each month
 
 current_year = int(datetime.date.today().strftime("%Y"))
 current_month = int(datetime.date.today().strftime("%m"))
@@ -13,16 +13,16 @@ today = int(datetime.date.today().strftime("%d"))
 
 end_of_this_month = monthrange(current_year, current_month)[1]
 
-if today < 25:
-	days_until_money = payday_is - today
+if today < payday:
+	days_until_money = payday - today
 	print "Wait %d more days for MONEY bith!" % days_until_money
 
-elif today == 25:
+elif today == payday:
 	print "It's PAYDAY bitch!"
 
-elif today > 25 and today <= end_of_this_month:
+elif today > payday and today <= end_of_this_month:
 	remaining_days_this_month = end_of_this_month - today
-	days_until_next_months_payday = remaining_days_this_month + payday_is
+	days_until_next_months_payday = remaining_days_this_month + payday
 
 	print "Wait for next month. Still %d more days to go!" % days_until_next_months_payday
 
